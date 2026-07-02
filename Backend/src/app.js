f890d79e915af955a841f8670caa00a2d67e39fa
Kartik <kartik.singh162008@gmail.com>
2026-07-02 20:12:34 +0530

    Update cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://fintrack-kartik.vercel.app"],
     credentials: true
 }));
 app.use(express.json());
