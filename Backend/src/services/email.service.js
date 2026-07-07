cddad84df5f8afd25c9af970dee831f8a482cdf3
Kartik <kartik.singh162008@gmail.com>
2026-07-07 21:36:45 +0530

    Fixes

--- Backend/src/services/email.service.js
+++ Backend/src/services/email.service.js
@@ -1,14 +1,29 @@
 const nodemailer = require('nodemailer');
 
+// const transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     type: 'OAuth2',
+//     user: process.env.GOOGLE_USER,
+//     clientId: process.env.GOOGLE_CLIENT_ID,
+//     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+//   },
+// });
+
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    type: 'OAuth2',
-    user: process.env.GOOGLE_USER,
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-  },
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    family: 4,
+    connectionTimeout: 10000,
+    auth: {
+        type: "OAuth2",
+        user: process.env.GOOGLE_USER,
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+    },
 });
 
 transporter.verify((error, success) => {
