b186a8a9283cf0c25538572d9be1362af9fba5a5
Kartik <kartik.singh162008@gmail.com>
2026-07-07 21:48:09 +0530

    Fix

--- Backend/src/services/email.service.js
+++ Backend/src/services/email.service.js
@@ -24,6 +24,9 @@ const transporter = nodemailer.createTransport({
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
     },
+    tls: {
+        rejectUnauthorized: false
+    }
 });
 
 transporter.verify((error, success) => {
