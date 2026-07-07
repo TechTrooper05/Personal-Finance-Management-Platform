e4de6cb6ddc7338720dcfa8e6f19711197c830d0
Kartik <kartik.singh162008@gmail.com>
2026-07-07 22:55:45 +0530

    Email Service

--- Backend/src/services/email.service.js
+++ Backend/src/services/email.service.js
@@ -1,15 +1,15 @@
-// const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
 
-// // const transporter = nodemailer.createTransport({
-// //   service: 'gmail',
-// //   auth: {
-// //     type: 'OAuth2',
-// //     user: process.env.GOOGLE_USER,
-// //     clientId: process.env.GOOGLE_CLIENT_ID,
-// //     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-// //     refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-// //   },
-// // });
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    type: 'OAuth2',
+    user: process.env.GOOGLE_USER,
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+  },
+});
 
 // const transporter = nodemailer.createTransport({
 //     host: "smtp.gmail.com",
@@ -29,48 +29,48 @@
 //     }
 // });
 
-// transporter.verify((error, success) => {
-//   if (error) {
-//     console.error('Error connecting to email server:', error);
-//   } else {
-//     console.log('Email server is ready to send messages');
-//   }
-// });
-
-// const sendEmail = async (to, subject, html) => {
-//     try {
-//         const info = await transporter.sendMail({
-//         from: `"FinTrack" <${process.env.GOOGLE_USER}>`, // sender address
-//         to, // list of receivers
-//         subject, // Subject line
-//         html, // html body
-//         });
-//     } catch (error) {
-//         console.error('Error sending email:', error);
-//         throw error;
-//     }
-// };
-
-// module.exports = {sendEmail};
-const { Resend } = require("resend");
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+transporter.verify((error, success) => {
+  if (error) {
+    console.error('Error connecting to email server:', error);
+  } else {
+    console.log('Email server is ready to send messages');
+  }
+});
 
 const sendEmail = async (to, subject, html) => {
     try {
-        const response = await resend.emails.send({
-            from: "FinTrack <onboarding@resend.dev>",
-            to: [to],
-            subject,
-            html,
+        const info = await transporter.sendMail({
+        from: `"FinTrack" <${process.env.GOOGLE_USER}>`, // sender address
+        to, // list of receivers
+        subject, // Subject line
+        html, // html body
         });
-
-        console.log("Email sent:", response);
-
     } catch (error) {
-        console.error("Error sending email:", error);
+        console.error('Error sending email:', error);
         throw error;
     }
 };
 
-module.exports = { sendEmail };
+module.exports = {sendEmail};
+// const { Resend } = require("resend");
+
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+// const sendEmail = async (to, subject, html) => {
+//     try {
+//         const response = await resend.emails.send({
+//             from: "FinTrack <onboarding@resend.dev>",
+//             to: [to],
+//             subject,
+//             html,
+//         });
+
+//         console.log("Email sent:", response);
+
+//     } catch (error) {
+//         console.error("Error sending email:", error);
+//         throw error;
+//     }
+// };
+
+// module.exports = { sendEmail };
